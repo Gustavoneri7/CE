635851e82d5b061bd336599deb9a8e1b92fab357
Gustavoneri7 <164513444+Gustavoneri7@users.noreply.github.com>
2024-03-30 14:55:35 -0300

    Add files via upload

---
+++ QUESTÃO 1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	
+
+   int x;
+   int y;
+   int z;	
+
+
+   x = 7+3*6/2-1;
+   y = 2%2 + 2*2/2;
+   z = (3*9*(3+(9*3/(3))));
+
+ 
+ cout << "valor de x:" << x << endl;
+ cout << "valor de y:" << y << endl;
+ cout << "valor de z:" << z << endl;
+ 
+ 	return 0;
+	
+}
